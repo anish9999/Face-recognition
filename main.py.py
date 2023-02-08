db95abc1ddb286f98a5803d8ec844e3614f98a30
anish9999 <anishshah9876@gmail.com>
2023-02-07 23:59:11 -0800

    opencv_headcount

--- main.py.py
+++ main.py.py
@@ -100,7 +100,7 @@ def argsParser():
     arg_parse = argparse.ArgumentParser()
     arg_parse.add_argument("-v", "--video", default=None, help="path to Video File ")
     arg_parse.add_argument("-i", "--image", default=None, help="path to Image File ")
-    arg_parse.add_argument("-c", "--camera", default=False, help="Set true if you want to use the camera.")
+    arg_parse.add_argument("-c", "--camera", default=True, help="Set true if you want to use the camera.")
     arg_parse.add_argument("-o", "--output", type=str, help="path to optional output video file")
     args = vars(arg_parse.parse_args())
 
